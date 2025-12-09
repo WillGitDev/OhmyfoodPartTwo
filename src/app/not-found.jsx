ba64e08933a282ec5167573dd7d9f71523376fcf
wil974-dev <wggron99@gmail.com>
2025-12-09 09:16:30 +0400

    Correction de l'affichage du message de la page 404 pour un message générale

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div className={styles.containerError}>
             <h1 className={styles.title}>Erreur 404</h1>
-            <p className={styles.content}>Ce restaurant n'existe pas</p>
+            <p className={styles.content}>Cette page n'existe pas</p>
         </div>
     );
 }
